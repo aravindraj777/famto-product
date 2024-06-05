69c81ca0822fd37358fbb26b84a457c7bfde875a
Aravind <aravindmunna240@gmail.com>
2024-06-05 17:45:58 +0530

    Initial commit

---
+++ src/test/java/com/famto/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.famto.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
